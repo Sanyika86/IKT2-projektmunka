97d047fd66fccb4983a27f86b5eadc82117fd39d
GodziXD <116083199+GodziXD@users.noreply.github.com>
2022-10-25 14:18:06 +0200

    Update feladat.py

--- feladat.py
+++ feladat.py
@@ -1,6 +1,5 @@
 #kérjünk be addig számokat amíg 500 és 1000 között van és páros!
 x = 0
-while x > 500 and x < 1000
+while x > 500 and x < 1000 and x % 2 == 0
   x = x + 1
-  if x % 2 == 0:
-    print(x)
+  print(x)
